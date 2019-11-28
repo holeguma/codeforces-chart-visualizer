6ec7ceccda231783413d8582f659e49d1fd7454f
holeguma <hmyyholeguma@gmail.com>
2019-11-28 13:27:31 +0900

    fixed import

--- getData.py
+++ getData.py
@@ -1,7 +1,7 @@
 import json
-from time import sleep
-
 import requests
+import os
+from time import sleep
 from tqdm import tqdm
 
 contestId = 566
@@ -13,24 +13,25 @@ json_data = response.json()
 
 
 def getData(count=5):
-    res=[]
+    res = []
     #新しくディレクトリを作ってここにデータを保存する
-    new_dir='./'+str(datetime.datetime.today().strftime("%Y%m%d"))
+    new_dir = './'+str(datetime.datetime.today().strftime("%Y%m%d"))
     os.makedirs(new_dir, exist_ok=True)
-    
-    for contestId in tqdm(range(1,count+1)):
+
+    for contestId in tqdm(range(1, count+1)):
         #print("contestId : "+str(contestId))
-        filename=str(contestId)+'.json'
-        url="https://codeforces.com/api/contest.standings?contestId="+str(contestId)+"&from=1&count="+str(count)+"&showUnofficial=true"
-        response=requests.get(url)
-        json_data=response.json()
-        
-        dumpFile(new_dir,filename,json_data)
+        filename = str(contestId)+'.json'
+        url = "https://codeforces.com/api/contest.standings?contestId=" + \
+            str(contestId)+"&from=1&count="+str(count)+"&showUnofficial=true"
+        response = requests.get(url)
+        json_data = response.json()
+
+        dumpFile(new_dir, filename, json_data)
         res.append(json_data)
-        if not contestId%5:
+        if not contestId % 5:
             sleep(1.5)
         #print(res)
-        
+
     return res
 
 
