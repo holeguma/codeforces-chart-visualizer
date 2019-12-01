38ee32951281f003a0d01f95956b659ce751cd9e
jumbo25 <snlingzinn@gmail.com>
2019-12-02 05:32:00 +0900

    make data of rankings by tag

---
+++ writeRanking.js
@@ -0,0 +1,42 @@
+
+var fs = require('fs');
+
+var cnt = 0;
+let n_contests = 1260;
+
+var hash = {};
+var ranking = {};
+var data = JSON.parse(fs.readFileSync("./user_data/hash_data.json", 'utf8'));
+hash = JSON.parse(JSON.stringify(data));
+var n_users = Object.keys(hash).length;
+//console.log(n_users);
+var names = Object.keys(hash);
+function compare(u1, u2) {
+    if (u1.rating < u2.rating) return 1;
+    else return -1;
+}
+async function makeRanking() {
+    for (var i = 0; i < n_users; i++) {
+        for (tag in hash[names[i]].tags) {
+            //console.log(tag);
+            if (!ranking[tag]) {
+                ranking[tag] = [];
+            }
+            ranking[tag].push({ userID: names[i], rating: hash[names[i]]["tags"][tag].value });
+            //console.log(ranking[tag]);
+        }
+        //console.log(hash[names[i]]);Node 
+    }
+    for (tag in ranking) {
+        console.log(tag);
+        ranking[tag].sort(compare);
+    }
+}
+
+makeRanking().then(function () {
+    var json_data = JSON.stringify(ranking);
+    fs.writeFileSync('ranking_data.json', json_data);
+})
+
+
+
